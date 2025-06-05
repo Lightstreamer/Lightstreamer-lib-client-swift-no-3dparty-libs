eb3f12981eeff80a60491c19235cfe89ef7e1d40
acarioni <alessandro.carioni@lightstreamer.com>
2025-06-05 09:16:14 +0200

    replace Starscream with URLSessionWebSocketTask to handle Websocket connections

--- Sources/LightstreamerClient/platform/LsWebsocket.swift
+++ Sources/LightstreamerClient/platform/LsWebsocket.swift
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import Foundation
-import Starscream
+//import Starscream
 
 typealias WSFactoryService = (NSRecursiveLock, String,
             String,
@@ -43,9 +43,178 @@ protocol LsWebsocketClient: AnyObject {
     func dispose()
 }
 
-class LsWebsocket: LsWebsocketClient {
+/// Delegate for URLSession
+class LsWebSocketSessionDelegate: NSObject, URLSessionWebSocketDelegate {
+    private let lock = NSRecursiveLock()
+    private weak var session: LsWebsocketSession?
+    
+    func setSession(_ session: LsWebsocketSession) {
+        synchronized {
+            self.session = session
+        }
+    }
+    
+    func getSession() -> LsWebsocketSession? {
+        synchronized {
+            return self.session
+        }
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        guard let taskWrapper = getSession()?.getWrapper(webSocketTask) else { return }
+        taskWrapper.getDelegate()?.onTaskOpen()
+    }
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        guard let taskWrapper = getSession()?.getWrapper(webSocketTask) else { return }
+        taskWrapper.getDelegate()?.onTaskError("unexpected disconnection: \(closeCode) - \(String(describing: reason))")
+    }
+    
+    private func synchronized<T>(block: () -> T) -> T {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
+        return block()
+    }
+}
+
+/// Wrapper of URLSession
+class LsWebsocketSession: NSObject, URLSessionWebSocketDelegate {
+    public static let shared = LsWebsocketSession()
+    
+    private let lock = NSRecursiveLock()
+    private let urlSession: URLSession
+    // URLSessionWebSocketTask does not provide event notifications for connection openings or disconnections.
+    // To track these events, I attach a URLSessionWebSocketDelegate to URLSession.
+    // When URLSession notifies such an event for a URLSessionWebSocketTask, this map helps retrieve the corresponding LsWebsocketTask wrapper.
+    private var taskMap = [URLSessionWebSocketTask: LsWebsocketTask]()
+    private var delegate = LsWebSocketSessionDelegate()
+    
+    public override init() {
+        urlSession = URLSession(configuration: .default, delegate: delegate, delegateQueue: nil)
+        super.init()
+        delegate.setSession(self)
+    }
+    
+    public func create(with request: URLRequest) -> LsWebsocketTask {
+        synchronized {
+            let task = urlSession.webSocketTask(with: request)
+            return LsWebsocketTask(task: task, session: self)
+        }
+    }
+    
+    func setWrapper(task: URLSessionWebSocketTask, wrapper: LsWebsocketTask) {
+        synchronized {
+            taskMap[task] = wrapper
+        }
+    }
+    
+    func getWrapper(_ task: URLSessionWebSocketTask) -> LsWebsocketTask? {
+        synchronized {
+            return taskMap[task]
+        }
+    }
+    
+    func disposeWrapper(_ task: URLSessionWebSocketTask) {
+        synchronized {
+            taskMap.removeValue(forKey: task)
+            return
+        }
+    }
+    
+    private func synchronized<T>(block: () -> T) -> T {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
+        return block()
+    }
+}
+
+/// Wrapper of URLSessionWebSocketTask
+class LsWebsocketTask {
+    private let lock = NSRecursiveLock()
+    private let session: LsWebsocketSession
+    private let task: URLSessionWebSocketTask
+    private var delegate: LsWebSocketTaskDelegate?
+    
+    public init(task: URLSessionWebSocketTask, session: LsWebsocketSession) {
+        self.session = session
+        self.task = task
+        session.setWrapper(task: task, wrapper: self)
+    }
+    
+    public func setDelegate(_ delegate: LsWebSocketTaskDelegate) {
+        synchronized {
+            self.delegate = delegate
+        }
+    }
+    
+    public func getDelegate() -> LsWebSocketTaskDelegate? {
+        synchronized {
+            return self.delegate
+        }
+    }
+    
+    public func connect() {
+        task.resume()
+        listen()
+    }
+    
+    private func listen()  {
+        task.receive { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let message):
+                switch message {
+                case .string(let chunk):
+                    for line in chunk.split(separator: "\r\n") {
+                        self.getDelegate()?.onTaskText(String(line))
+                    }
+                case .data(_):
+                    fatalError("Unexpect message type")
+                @unknown default:
+                    fatalError("Unexpect message type")
+                }
+            case .failure(let error):
+                self.getDelegate()?.onTaskError(error.localizedDescription)
+            }
+            self.listen()
+        }
+    }
+    
+    public func disconnect() {
+        task.cancel()
+        session.disposeWrapper(task)
+    }
+    
+    public func write(string text: String) {
+        task.send(.string(text)) { [weak self] error in
+            guard let error = error, let self = self else { return }
+            self.getDelegate()?.onTaskError(error.localizedDescription)
+        }
+    }
+    
+    private func synchronized<T>(block: () -> T) -> T {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
+        return block()
+    }
+}
+
+/// Delegate responsible for publishing events from LsWebsocketTask
+protocol LsWebSocketTaskDelegate: AnyObject {
+    func onTaskOpen()
+    func onTaskText(_ text: String)
+    func onTaskError(_ error: String)
+}
+
+class LsWebsocket: LsWebsocketClient, LsWebSocketTaskDelegate {
     let lock: NSRecursiveLock
-    let socket: WebSocket
+    let socket: LsWebsocketTask
     let onOpen: (LsWebsocket) -> Void
     let onText: (LsWebsocket, String) -> Void
     let onError: (LsWebsocket, String) -> Void
@@ -67,13 +236,18 @@ class LsWebsocket: LsWebsocketClient {
             request.setValue(val, forHTTPHeaderField: key)
         }
         if streamLogger.isDebugEnabled {
-            streamLogger.debug("WS connecting: \(request) \(request.headers)")
+            streamLogger.debug("WS connecting: \(request) \(request.allHTTPHeaderFields ?? [:])")
         }
+        /*
         socket = WebSocket(request: request)
         socket.callbackQueue = defaultQueue
         socket.onEvent = { [weak self] e in
             self?.onEvent(e)
         }
+        socket.connect()
+        */
+        self.socket = LsWebsocketSession.shared.create(with: request)
+        socket.setDelegate(self)
         socket.connect()
     }
     
@@ -102,6 +276,28 @@ class LsWebsocket: LsWebsocketClient {
         }
     }
     
+    func onTaskOpen() {
+        defaultQueue.async { [weak self] in
+            guard let self = self else { return }
+            onOpen(self)
+        }
+    }
+    
+    func onTaskText(_ text: String) {
+        defaultQueue.async { [weak self] in
+            guard let self = self else { return }
+            onText(self, text)
+        }
+    }
+    
+    func onTaskError(_ error: String) {
+        defaultQueue.async { [weak self] in
+            guard let self = self else { return }
+            onError(self, error)
+        }
+    }
+    
+    /*
     private func onEvent(_ event: WebSocketEvent) {
         synchronized {
             guard !m_disposed else {
@@ -140,6 +336,7 @@ class LsWebsocket: LsWebsocketClient {
             }
         }
     }
+    */
 
     private func synchronized<T>(block: () -> T) -> T {
         lock.lock()
